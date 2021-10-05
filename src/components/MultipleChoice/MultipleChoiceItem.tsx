08b461587f84854c610cf594642dfdab2ac3e5ef
Shahriat Hossain <somel90@gmail.com>
2021-10-05 08:03:09 +0600

    implemented react a11y

--- src/components/MultipleChoice/MultipleChoiceItem.tsx
+++ src/components/MultipleChoice/MultipleChoiceItem.tsx
@@ -13,23 +13,38 @@ const MultipleChoiceItem: React.FC<{
 }> = (props) => {
   const quesCtx = useContext(QuestionnaireContext);
 
-  const clickHandler = () => {
+  const onClickHandler = () => {
+    addAnswerCurrentItemIndex();
+  };
+
+  const onKeyDownHandler = () => {
+    addAnswerCurrentItemIndex();
+  };
+
+  const addAnswerCurrentItemIndex = () => {
     quesCtx.addAnswer(props.questionIdentifier, props.choiceId);
     quesCtx.addCurrentItemIdx(quesCtx.currentItemIdx + 1);
   };
 
   return (
-    <li
-      className={
-        props.multipleChoice.selected
-          ? "question__choice question__choice--selected"
-          : "question__choice"
-      }
-      onClick={clickHandler}
-    >
-      <ChoiceOrder isActive={props.multipleChoice.selected} letter={numToSSColumn(props.choiceId + 1)}></ChoiceOrder>
-      <ChoiceContent content={props.multipleChoice.label}></ChoiceContent>
-      <ChoiceChecked isActive={props.multipleChoice.selected}></ChoiceChecked>
+    <li>
+      <div
+        role="presentation"
+        onClick={onClickHandler}
+        onKeyDown={onKeyDownHandler}
+        className={
+          props.multipleChoice.selected
+            ? "question__choice question__choice--selected"
+            : "question__choice"
+        }
+      >
+        <ChoiceOrder
+          isActive={props.multipleChoice.selected}
+          letter={numToSSColumn(props.choiceId + 1)}
+        ></ChoiceOrder>
+        <ChoiceContent content={props.multipleChoice.label}></ChoiceContent>
+        <ChoiceChecked isActive={props.multipleChoice.selected}></ChoiceChecked>
+      </div>
     </li>
   );
 };
